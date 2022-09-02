60ca3fe7a7911e312b2adaee881b59321226676b
Daniela Suárez <cdanisuarez@users.noreply.github.com>
2022-09-02 13:33:28 -0500

    Topping option added

--- src/pages/entry/test/Options.test.jsx
+++ src/pages/entry/test/Options.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Options from '../Options';
 
-test('Displays inage for each scoop from server', async () => {
+test('Displays image for each scoop from server', async () => {
   render(<Options optionType="scoops" />);
 
   const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
@@ -9,4 +9,18 @@ test('Displays inage for each scoop from server', async () => {
 
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
+});
+
+test('Displays image for each tooping from server', async () => {
+  render(<Options optionType="toppings" />);
+
+  const scoopImages = await screen.findAllByRole('img', { name: /topping$/i });
+  expect(scoopImages).toHaveLength(3);
+
+  const altText = scoopImages.map((element) => element.alt);
+  expect(altText).toEqual([
+    'Cherries topping',
+    'M&Ms topping',
+    'Hot fudge topping',
+  ]);
 });
